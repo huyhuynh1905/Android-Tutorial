77eb69c87c2c6ebc220f5880ed6a762e56990436
Huy Huỳnh <34849208+huyhuynh1905@users.noreply.github.com>
2019-07-05 21:06:00 +0700

    Chappter 9-10: ImageView and Change Background

---
+++ ImageView/app/src/main/java/android/huyhuynh/imageview/MainActivity.java
@@ -0,0 +1,62 @@
+package android.huyhuynh.imageview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView imgView;
+    Button btnChange;
+    ConstraintLayout layoutBackgr;
+    List<Integer> ls;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        init();
+        control();
+
+    }
+
+    private void init() {
+        imgView = findViewById(R.id.imageView);
+        btnChange = findViewById(R.id.btnBackground);
+        layoutBackgr = findViewById(R.id.layoutBack);
+        ls = new ArrayList<>();
+        ls.add(R.drawable.ha1);
+        ls.add(R.drawable.ha2);
+        ls.add(R.drawable.ha3);
+        ls.add(R.drawable.ha4);
+    }
+
+    private void control() {
+        imgView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Collections.shuffle(ls);
+                layoutBackgr.setBackgroundResource(ls.get(0));
+            }
+        });
+        btnChange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int red = new Random().nextInt(255);
+                int green = new Random().nextInt(255);
+                int blue = new Random().nextInt(255);
+                layoutBackgr.setBackgroundColor(Color.rgb(red,green,blue));
+            }
+        });
+    }
+}
