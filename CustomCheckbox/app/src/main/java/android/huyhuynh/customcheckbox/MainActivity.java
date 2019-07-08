dfd060e9144aaf1d2da01bfbc28a0580c0af0710
Huy Huỳnh <34849208+huyhuynh1905@users.noreply.github.com>
2019-07-08 21:52:00 +0700

    Chapter 25: Custom Checkbox

---
+++ CustomCheckbox/app/src/main/java/android/huyhuynh/customcheckbox/MainActivity.java
@@ -0,0 +1,15 @@
+package android.huyhuynh.customcheckbox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Nhớ resize kích thước trước nhé, hình mình chưa resize nên hơi lớn :P
+    }
+}
