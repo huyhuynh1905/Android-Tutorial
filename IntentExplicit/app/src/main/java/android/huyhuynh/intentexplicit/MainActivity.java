a41d37b5d853b7866fb8fd3d98273db15ae44a41
Huy Huỳnh <34849208+huyhuynh1905@users.noreply.github.com>
2019-07-11 22:37:00 +0700

    Chapter 38,39: Send String,Int,Array IntentExplicit

--- IntentExplicit/app/src/main/java/android/huyhuynh/intentexplicit/MainActivity.java
+++ IntentExplicit/app/src/main/java/android/huyhuynh/intentexplicit/MainActivity.java
@@ -6,9 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     Button btnMainAc;
+    EditText edtNum, edtText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +18,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnMainAc = findViewById(R.id.btnMainAc);
+        edtText = findViewById(R.id.edtText);
+        edtNum = findViewById(R.id.edtNum);
 
         btnMainAc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent mainIntent = new Intent(MainActivity.this,SecondActivity.class);
+
+//                String text = edtText.getText().toString().trim();
+//                int num = Integer.parseInt(edtNum.getText().toString().trim());
+//
+//                mainIntent.putExtra("Str",text);
+//                mainIntent.putExtra("Num",num);
+                String[] arrStr = {"Hcode","Java","Android"};
+                mainIntent.putExtra("Array",arrStr);
+
                 startActivity(mainIntent);
             }
         });
--- IntentExplicit/app/src/main/java/android/huyhuynh/intentexplicit/SecondActivity.java
+++ IntentExplicit/app/src/main/java/android/huyhuynh/intentexplicit/SecondActivity.java
@@ -6,9 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
     Button btnSecondAc;
+    TextView txtShow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +18,17 @@ public class SecondActivity extends AppCompatActivity {
         setContentView(R.layout.activity_second);
 
         btnSecondAc = findViewById(R.id.btnSecondAc);
+        txtShow = findViewById(R.id.txtShow);
+
+        //Receive
+        Intent receInten = getIntent();
+//        String strRece = receInten.getStringExtra("Str");
+//        int numRece = receInten.getIntExtra("Num",0);
+//
+//        txtShow.setText("Text: "+strRece+"\n"+"Number: "+numRece);
+        String[] arrRece = receInten.getStringArrayExtra("Array");
+        txtShow.setText(arrRece.length+" - "+arrRece[0]);
+
 
         btnSecondAc.setOnClickListener(new View.OnClickListener() {
             @Override
