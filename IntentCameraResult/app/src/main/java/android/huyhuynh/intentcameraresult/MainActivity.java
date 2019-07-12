fb0f6fa3fbe4695462e2f70f36a7995a45ed6a79
Huy Huỳnh <34849208+huyhuynh1905@users.noreply.github.com>
2019-07-12 21:46:54 +0700

    Chapter 44 Intent Camera Result With Permission

--- IntentCameraResult/app/src/main/java/android/huyhuynh/intentcameraresult/MainActivity.java
+++ IntentCameraResult/app/src/main/java/android/huyhuynh/intentcameraresult/MainActivity.java
@@ -1,9 +1,13 @@
 package android.huyhuynh.intentcameraresult;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -25,10 +29,19 @@ public class MainActivity extends AppCompatActivity {
         btnCam.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(intent,REQUEST_CODE_IMG);
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.CAMERA},REQUEST_CODE_IMG);
             }
         });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode==REQUEST_CODE_IMG && permissions.length>0
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(intent,REQUEST_CODE_IMG);
+        }
     }
 
     @Override
