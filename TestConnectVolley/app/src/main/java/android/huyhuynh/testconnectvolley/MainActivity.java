03794f5a189885d92aa48e75debd45150e8bc3b2
Huy Huỳnh <34849208+huyhuynh1905@users.noreply.github.com>
2019-07-19 21:14:23 +0700

    Test Volley JSON Online

---
+++ TestConnectVolley/app/src/main/java/android/huyhuynh/testconnectvolley/MainActivity.java
@@ -0,0 +1,43 @@
+package android.huyhuynh.testconnectvolley;
+/**
+ * Created by Huy Huynh on 07/19/19.
+ */
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        String url = "http://192.168.1.102/androidstudio/creatJSON.php";
+        JsonArrayRequest arrayRequest = new JsonArrayRequest(Request.Method.POST, url, null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                Toast.makeText(MainActivity.this,response.toString(),Toast.LENGTH_LONG).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this,error.toString(),Toast.LENGTH_LONG).show();
+                Log.d("Err", "onErrorResponse: "+error.toString());
+            }
+        });
+        requestQueue.add(arrayRequest);
+    }
+}
