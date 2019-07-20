63a9244eba32758aaa8fe140b074ef6c0b77dde9
Huy Huỳnh <34849208+huyhuynh1905@users.noreply.github.com>
2019-07-20 22:41:37 +0700

    Chapter 63: Fragment Add by Code

--- StartFragment/app/src/main/java/android/huyhuynh/startfragment/MainActivity.java
+++ StartFragment/app/src/main/java/android/huyhuynh/startfragment/MainActivity.java
@@ -1,6 +1,8 @@
 package android.huyhuynh.startfragment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Thêm fragment bằng code:
+        //2 dòng mặc định phải có
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        //Thêm fragment vào
+        FragmentA fragmentA = new FragmentA();
+        fragmentTransaction.add(R.id.frameLayout,fragmentA);
+        fragmentTransaction.commit(); //Xác nhận
     }
 }
