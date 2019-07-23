ea30e505298825ea69b9676621cd42c7b6fc0911
Huy Huỳnh <34849208+huyhuynh1905@users.noreply.github.com>
2019-07-23 20:55:38 +0700

    API Youtube 2: Play video

--- APIYouTube/app/src/main/java/android/huyhuynh/apiyoutube/MainActivity.java
+++ APIYouTube/app/src/main/java/android/huyhuynh/apiyoutube/MainActivity.java
@@ -1,17 +1,52 @@
 package android.huyhuynh.apiyoutube;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
 
-public class MainActivity extends YouTubeBaseActivity {
+public class MainActivity extends YouTubeBaseActivity
+        implements YouTubePlayer.OnInitializedListener {
+
+    YouTubePlayerView mYouTubePlayerView;
 
     String API_KEY = "AIzaSyAUD_hrm-ZxChtofp4ORCYzImx38NQm61Q";
+    int REQUEST_ERR = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mYouTubePlayerView = findViewById(R.id.youtubeView);
+        mYouTubePlayerView.initialize(API_KEY,this);
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+        youTubePlayer.cueVideo("DNrNBLI3d68");
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider,
+                                        YouTubeInitializationResult youTubeInitializationResult) {
+        if (youTubeInitializationResult.isUserRecoverableError()){
+            youTubeInitializationResult.getErrorDialog(MainActivity.this,REQUEST_ERR);
+        }else {
+            Toast.makeText(MainActivity.this,"ERR",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode==REQUEST_ERR){
+            mYouTubePlayerView.initialize(API_KEY,MainActivity.this);
+        }
     }
 }
