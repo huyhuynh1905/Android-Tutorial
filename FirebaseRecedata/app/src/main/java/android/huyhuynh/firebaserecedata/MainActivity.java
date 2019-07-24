ac04babe8abd538d1ae8c11f896ad8a1d2577402
Huy Huỳnh <34849208+huyhuynh1905@users.noreply.github.com>
2019-07-24 23:49:12 +0700

    Firebase Receive 1: Create and connect

---
+++ FirebaseRecedata/app/src/main/java/android/huyhuynh/firebaserecedata/MainActivity.java
@@ -0,0 +1,20 @@
+package android.huyhuynh.firebaserecedata;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+    DatabaseReference mDatabaseReference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mDatabaseReference = FirebaseDatabase.getInstance().getReference();
+        mDatabaseReference.child("KhoaHoc").setValue("Lập Trình Android");
+    }
+}
