9fd7b71f27ddb3a8434d450694e05428f4194179
Huy Huỳnh <34849208+huyhuynh1905@users.noreply.github.com>
2019-07-24 23:11:18 +0700

    Firebase 2: Save data setValue

--- FirebaseDemo/app/src/main/java/android/huyhuynh/firebasedemo/MainActivity.java
+++ FirebaseDemo/app/src/main/java/android/huyhuynh/firebasedemo/MainActivity.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+
+    DatabaseReference mDatabaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Khởi tạo
+        mDatabaseReference = FirebaseDatabase.getInstance().getReference();
+        mDatabaseReference.child("HoTen").setValue("Huỳnh Bảo Huy");
     }
 }
